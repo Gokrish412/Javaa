fe5242bb9d24e6ba18fcc815760d33e6497fb666
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-10 13:15:48 +0530

    Create Myage.java

---
+++ OOPS/Myage.java
@@ -0,0 +1,8 @@
+package OOPS;
+public class Myage {
+    int x=21;
+    public static void main (String args []) {
+        Myage age= new Myage ();
+        System.out.println(age.x);
+    }
+    }
