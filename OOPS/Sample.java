59bc2efe08e17c63a1ae3c96b1e221d0ba474422
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-10 13:25:45 +0530

    Create Sample.java

---
+++ OOPS/Sample.java
@@ -0,0 +1,16 @@
+package OOPS;
+class Basic 
+{
+    void output() 
+       { 
+           System.out.println("Basic OOPS Concept"); 
+       } 
+} 
+class Sample
+{ 
+    public static void main(String args[]) 
+       {   
+          Basic check = new Basic(); 
+          check.output();  
+       } 
+}
