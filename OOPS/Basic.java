c5c3f9ee97bfac76181414a868ea3428ee1a36fd
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-10 20:28:58 +0530

    Create Basic.java

---
+++ OOPS/Basic.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+class Basic{
+    int numb=9;
+    String name="Gokul";
+public static void main(String args[]){
+    Basic basic=new Basic();
+    System.out.println(“Roll No”+basic.numb);
+    System.out.println(“Name”+basic.name);
+}
+}
