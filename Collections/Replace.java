3292988ecc452c7943fec1e999cf6a86945e3276
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-15 12:09:47 +0530

    Create Replace.java

---
+++ Collections/Replace.java
@@ -0,0 +1,14 @@
+package Collections;
+import java.util.*;
+class Replace{  
+   public static void main(String args[]){  
+        
+      ArrayList<String> gym=new ArrayList<String>();  
+      gym.add("Dumbbell");
+      gym.add("Situpbar");
+      gym.add("Forearm Strengthner");
+      gym.add("Weights");
+      gym.add(1,"Pushup board");
+      System.out.println(gym);
+}
+}
