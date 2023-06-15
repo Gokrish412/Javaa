47e132713d05f53b217ffad3ddfeb91287f8eda3
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-15 12:24:04 +0530

    Create get.java

---
+++ Collections/get.java
@@ -0,0 +1,15 @@
+package Collections;
+import java.util.*;
+class get{  
+   public static void main(String args[]){  
+        
+      ArrayList<String> gym=new ArrayList<String>();  
+      gym.add("Dumbbell");
+      gym.add("Situpbar");
+      gym.add("Forearm Strengthner");
+      gym.add("Weights");
+      gym.add("Barbell");
+      gym.add("Powerack");
+      System.out.println(gym.get(3));
+}
+}
