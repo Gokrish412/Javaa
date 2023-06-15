44bbfaee6a367c88b46f7380b1114c23f6c8c6ca
Gokrish412 <91113296+Gokrish412@users.noreply.github.com>
2023-06-15 12:09:15 +0530

    Create Basic.java

---
+++ Collections/Basic.java
@@ -0,0 +1,13 @@
+package Collections;
+import java.util.*;
+class Basic{  
+   public static void main(String args[]){  
+        
+      ArrayList<String> gym=new ArrayList<String>();  
+      gym.add("Dumbbell");
+      gym.add("Situpbar");
+      gym.add("Forearm Strengthner");
+      gym.add("Weights");
+      System.out.println(gym);
+}
+}
